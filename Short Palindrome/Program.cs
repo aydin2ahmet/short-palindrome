e185717d0c361b67e974e00c6299542d211d5bc8
AHMET AYDIN <ahmet.aydin@visiomex.com>
2022-06-20 18:56:44 +0300

    solving commit

---
+++ Short Palindrome/Program.cs
@@ -0,0 +1,30 @@
+﻿var result = shortPalindrome("kkkkkkz");
+
+Console.WriteLine(result);
+
+long shortPalindrome(string s)
+{
+    int[] firstArray = new int[26];
+    long[,] secondArray = new long[26, 26];
+    long[,,] thirdArray = new long[26, 26, 26];
+    long totalCount = 0;
+
+    long modValue = (long)Math.Pow(10, 9) + 7;
+
+    foreach (var i in s)
+    {
+        var k = (int)i - 97;
+
+        for (int j = 0; j < 26; j++)
+        {
+            totalCount += thirdArray[k, j, j];
+            thirdArray[j, k, k] = (thirdArray[j, k, k] + secondArray[j, k]) % modValue;
+            secondArray[j, k] = (secondArray[j, k] + firstArray[j]) % modValue;
+        }
+
+        totalCount %= modValue;
+        firstArray[k] += 1;
+    }
+
+    return totalCount;
+}
